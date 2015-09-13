d97b382023a88f84a34e7381c95dd08fe12e1e68
Sleeeepy <elmarzimmermann@hotmail.com>
2015-09-14 00:26:49 +0100

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/ring-dev'
+  },
+
+  seedDB: true
+};
